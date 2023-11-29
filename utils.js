fd6f00c3ee57b1e25862615ff3364b12ff6463fc
ivreznap <68358383+ivreznap@users.noreply.github.com>
2023-11-29 11:55:07 +0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,21 +1,22 @@
+// util.js
+
 import GLib from 'gi://GLib';
 
-export const getLanIp = () => {       
-    // Ask the IP stack what route would be used to reach 1.1.1.1 (Cloudflare DNS)
-    // Specifically, what src would be used for the 1st hop?
-    const command_output_bytes = GLib.spawn_command_line_sync('ip route get 1.1.1.1')[1];
-    const command_output_string = String.fromCharCode.apply(null,  command_output_bytes);
+export const getLanIpOrTun0 = () => {
+    const commandOutputBytes = GLib.spawn_command_line_sync('ip addr show tun0 | grep -Po \'inet \K[\d.]+\'')[1];
+    const tun0Ip = String.fromCharCode.apply(null, commandOutputBytes).trim();
 
-    // Output of the "ip route" command will be a string
-    // " ... src 1.2.3.4 ..."
-    // So basically we want the next token (word) immediately after the "src"
-    // word, and nothing else. This is considerd our LAN IP address.
-    const Re = new RegExp(/src [^ ]+/g);
-    const matches = command_output_string.match(Re);
-    if (matches) {
-        return matches[0].split(' ')[1];
+    if (tun0Ip) {
+        return tun0Ip;
     } else {
-        return '';
+        const lanIpCommandOutputBytes = GLib.spawn_command_line_sync('ip route get 1.1.1.1')[1];
+        const lanIpCommandOutputString = String.fromCharCode.apply(null, lanIpCommandOutputBytes);
+        const Re = new RegExp(/src [^ ]+/g);
+        const matches = lanIpCommandOutputString.match(Re);
+        if (matches) {
+            return matches[0].split(' ')[1];
+        } else {
+            return '';
+        }
     }
-}
-
+};
